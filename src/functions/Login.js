ca2b8444823e3ae22af5b1f7deb36bbfbb101833
Bening Haqi <haqi@luminary.com>
2024-11-07 08:39:48 +0700

    update

--- src/functions/Login.js
+++ src/functions/Login.js
@@ -45,7 +45,7 @@ app.http('Login', {
     } catch (error) {
         context.log('Database error:', error);
         context.res = { status: 500, 
-                        body: {message: "Server error", data: {} }};
+                        body: {message: "Server error", error: error }};
     }
 
         return {
